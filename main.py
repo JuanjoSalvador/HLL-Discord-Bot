b2097c6923e894851c98973db24d675ecbe415bb
Juanjo Salvador <juanjosalvador@netc.eu>
2020-08-10 13:39:02 +0200

    Configurada reconexión del servidor

--- main.py
+++ main.py
@@ -1,24 +1,90 @@
 import os
-import json
-import random
+import discord
+import datetime
 import requests
+import logging
 
-from discord.ext import commands
+from random import randrange
+
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions, CheckFailure
+
 from dotenv import load_dotenv
 
+''' 
+TO-DO
+
+* Añadir soporte para Redis
+* Configurar un segundo worker que se ejecute cada minuto y guarde métricas en Redis si se cumplen las condiciones (al menos 1 player)
+* Todas las peticiones que se ejecutan aquí, deben ser consultas a los registros de Redis, 
+'''
+
+logging.basicConfig(level=logging.INFO)
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-SERVER_ID = os.getenv('SERVER_ID')
+# Esta información tiene que escribirse en el .env una vez se escriba
+config = {
+    'token': os.getenv('DISCORD_TOKEN'),
+    'server_id': os.getenv('SERVER_ID'),
+    'channel_id': ''
+}
+
+#GUILD_ID = ''
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='players')
-async def player_count(ctx):
-    r = requests.get(f'https://api.battlemetrics.com/servers/{SERVER_ID}')
-    data = r.json()
-    players = data['data']['attributes']['players']
-    current_map = data['data']['attributes']['details']['map']
-    response = f'Ahora mismo hay **{players} jugadores**, en el mapa **{current_map}**'
-    await ctx.send(response)
+@bot.event
+async def on_ready():
+    #data = requests.get(SERVER_METRICS).json()
+    if config['channel_id']:
+        get_data.start()
+    print("Bot arriba y funcionando.")
+    print(bot.user)
+    # Guarda los datos en REDIS
 
-bot.run(TOKEN)
+@bot.event
+async def on_disconnect():
+    date = datetime.datetime.now()
+    print(f'Señal de desconexión recibida a las {date}')
+    print("Desconectando...")
+
+@bot.event
+async def on_resumed():
+    date = datetime.datetime.now()
+    print(f'Reconectado a las {date}')
+
+# CONFIG COMMANDS
+@bot.command(name='start', pass_context=True)
+@has_permissions(administrator=True)
+async def start_bot(ctx, channel_id):
+    guild_id = ctx.message.guild.id
+    # Aquí lo ideal sería guardar esto en una base de datos persistente o escribirla en el .env del bot
+    config['channel_id'] = int(channel_id)
+    try:
+        get_data.start()
+        await ctx.send('Inicializado correctamente. Utiliza el comando !setchannel \
+<channel_id> para cambiar el canal de voz donde se mostrarán los datos del servidor.')
+    except Exception as ex:
+        await ctx.send('Ocurrió un error. Revisa los logs del servidor para \
+más información.')
+
+@tasks.loop(minutes=5.0)
+async def get_data():
+    channel_players = bot.get_channel(config['channel_id'])
+    server_id = config['server_id']
+
+    data = requests.get(f'https://api.battlemetrics.com/servers/{server_id}').json()
+    current_map = data['data']['attributes']['details']['map']
+    players = data['data']['attributes']['players']
+    if players > 0:
+        await channel_players.edit(name=f'{players}/100 - {current_map}')
+    date = datetime.datetime.now()
+    bm_data = data['data']['attributes']['updatedAt']
+    print(f'Actualizado: [{date}] {current_map} {players}/100')
+    print(f'BattleMetrics: {bm_data}')
+
+try:
+    bot.run(config['token'], reconnect=True)
+
+except Exception as ex:
+    print(f'Ocurrió un error! {ex}')
